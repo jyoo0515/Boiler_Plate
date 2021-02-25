7c38dea2299b3b301cb0348eeeb58653d276e5ef
jyoo0515 <jasonyoo0515@gmail.com>
2021-02-25 22:24:35 +0900

    directory change

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ mongoose.connect(config.mongoURI, {
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// app.get('/', (req, res) => res.send('Hello World!'))
 
 app.post('/api/users/register', (req, res) => {
   const user = new User(req.body)
